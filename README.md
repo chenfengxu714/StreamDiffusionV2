ca14464a2c65db17a77e2e9b2d0b16a218722082
Tianrui Feng <jerryfeng2003@gmail.com>
2025-07-06 12:10:34 -0700

    Upload DCM

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # StreamDiffusionV2
+
+Installation
+```shell
+conda create -n dcm python=3.10.0
+conda activate dcm
+git clone https://github.com/Vchitect/DCM
+cd DCM
+pip install torch==2.6.0 torchvision==0.21.0 torchaudio==2.6.0 --index-url https://download.pytorch.org/whl/cu126
+pip install -e .
+pip install flash-attn --no-build-isolation
+```
+
+Download Checkpoints
+```shell
+huggingface-cli download --resume-download cszy98/DCM --local-dir ./ckpt
+```
---
+++ scripts/distill/distill_hy_detail_expert.sh
@@ -0,0 +1,43 @@
+export WANDB_BASE_URL="https://api.wandb.ai"
+export WANDB_MODE=online
+
+torchrun --nnodes 3 --nproc_per_node 8\
+    --node_rank=0 \
+    --rdzv_id=456 \
+    --rdzv_backend=c10d \
+    --rdzv_endpoint=10.140.54.18:23209 \
+    fastvideo/distill_dcm_hy_detail_expert.py \
+    --seed 42\
+    --pretrained_model_name_or_path path_to_semantic_expert \
+    --model_type hunyuan_hf \
+    --cache_dir data/.cache \
+    --data_json_path data/HD-Mixkit-Finetune-Hunyuan/videos2caption.json \
+    --validation_prompt_dir data/HD-Mixkit-Finetune-Hunyuan/validation \
+    --gradient_checkpointing \
+    --train_batch_size 1 \
+    --num_latent_t 16 \
+    --sp_size 4 \
+    --train_sp_batch_size 1 \
+    --dataloader_num_workers 4 \
+    --gradient_accumulation_steps 1 \
+    --max_train_steps 1000 \
+    --learning_rate 5e-6 \
+    --mixed_precision bf16 \
+    --checkpointing_steps 64 \
+    --validation_steps 64 \
+    --validation_sampling_steps "2,4,8" \
+    --checkpoints_total_limit 2 \
+    --allow_tf32 \
+    --ema_start_step 0 \
+    --cfg 0.0 \
+    --log_validation \
+    --output_dir data/outputs/distill_hy_detail \
+    --tracker_project_name distill_hy_detail \
+    --num_height 720 \
+    --num_width 1280 \
+    --num_frames 29 \
+    --validation_guidance_scale "1.0" \
+    --shift 17 \
+    --num_euler_timesteps 50 \
+    --multi_phased_distill_schedule "4000-1" \
+    --not_apply_cfg_solver --use_cpu_offload 
