ef45efc942717e0ab536d8221632e0c49f92e876
Tianrui Feng <jerryfeng2003@gmail.com>
2025-10-06 23:33:27 +0800

    Delete inference.sh

--- inference.sh
+++
@@ -1,5 +0,0 @@
-CUDA_VISIBLE_DEVICES=1 python minimal_inference/autoregressive_inference.py \
---config_path configs/wan_causal_dmd.yaml \
---checkpoint_folder ckpts/autoregressive_checkpoint \
---output_folder outputs/ \
---prompt_file_path prompt.txt \
